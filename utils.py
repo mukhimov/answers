48ddfbc74235d34c3c4a512606bf599ed6500d28
mukhimov_701 <u900552932@gmail.com>
2025-12-04 22:53:31 -0800

    initial commit

---
+++ utils.py
@@ -0,0 +1,34 @@
+# 1-topshiriq
+
+def sonning_qoshma_yigindisi(a,b):
+    return a + b
+# natija = sonni_yigindisi(20, 30)
+# print("yig'indi:>", natija)
+
+# 2-topshiriq
+
+def sonning_ayirma_yigindi(a,b):
+    return a - b
+# natija = yigindi(20, 30)
+# print("Yig'indi:>", natija)
+
+# 3-topshiriq
+
+def sonning_kopaytma_yigindi(a,b):
+    return a * b
+# natija = yigindi(20, 30)
+# print("Yig'indi:>", natija)
+
+# 4-topshiriq
+
+def sonning_bolish_yigindi(a,b):
+    return a // b
+# natija = yigindi(2, 2)
+# print("Yig'indi:>", natija)
+
+# 5-topshiriq
+
+def sonning_daraja_yigindi(a,b):
+    return a ** b
+# natija = yigindi(2, 5)
+# print("Yig'indi:>", natija)
